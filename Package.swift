swift-primitives/swift-cpu-primitives

Narrow import precision: umbrella → specific variant products
Replace banned primary-decomposition umbrella imports with specific
variant products per [MOD-015] consumer import precision.

Package.swift
@@ -31,7 +31,8 @@ let package = Package(
             name: "CPU Primitives",
             dependencies: [
                 .target(name: "CCPUShim"),
-                .product(name: "Binary Primitives", package: "swift-binary-primitives"),
+                .product(name: "Binary Primitives Core", package: "swift-binary-primitives"),
+                .product(name: "Binary Serializable Primitives", package: "swift-binary-primitives"),
                 .product(name: "Bit Primitives", package: "swift-bit-primitives"),
                 .product(name: "Dimension Primitives", package: "swift-dimension-primitives"),
             ]

swift-primitives/swift-dimension-primitives

Package.swift
@@ -33,7 +33,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Finite Primitives", package: "swift-finite-primitives"),
                 .product(name: "Identity Primitives", package: "swift-identity-primitives"),
-                .product(name: "Numeric Primitives", package: "swift-numeric-primitives"),
+                .product(name: "Numeric Primitives Core", package: "swift-numeric-primitives"),
                 .product(name: "Real Primitives", package: "swift-numeric-primitives"),
                 .product(name: "Formatting Primitives", package: "swift-formatting-primitives")
             ]